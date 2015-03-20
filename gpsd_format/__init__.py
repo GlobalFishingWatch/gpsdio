2411747e6ff2770c2c05a73d82341a9f25813030
Kevin Wurster <kevin@skytruth.org>
2015-03-20 13:02:27 -0400

    Added __author__, __license__, etc.

---
+++ gpsd_format/__init__.py
@@ -0,0 +1,33 @@
+# encoding:utf-8
+
+
+"""
+A collection of tools for working with the GPSD JSON format (or the same format in a msgpack container).
+"""
+
+
+import schema
+import validate
+
+
+__version__ = "0.1"
+__author__ = "Egil Moeller, Kevin Wurster"
+__email__ = "egil@skytruth.org, kevin@skytruth.org"
+__license__ = """
+Copyright 2014-2015 SkyTruth
+Authors:
+Egil Möller <egil@skytruth-org>
+Kevin Wurster <kevin@skytruth.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
