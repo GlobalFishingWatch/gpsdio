419641d6d863f05151d24a3951628f50534f020f
Kevin Wurster <kevin@skytruth.org>
2015-04-07 12:04:06 -0400

    Merging driver auto-registration.

--- tests/test_core.py
+++ tests/test_core.py
@@ -128,7 +128,7 @@ class TestStream(unittest.TestCase):
 
 def test_get_driver():
 
-    for d in gpsd_format.drivers.REGISTERED_DRIVERS:
+    for d in [_d for _d in gpsd_format.drivers.BaseDriver.by_name.values() if not _d.compression]:
         rd = gpsd_format.drivers.get_driver(d.name)
         assert rd == d, "%r != %r" % (d, rd)
     try:
@@ -140,7 +140,7 @@ def test_get_driver():
 
 def test_get_compression():
 
-    for c in gpsd_format.drivers.COMPRESSION_DRIVERS:
+    for c in [_d for _d in gpsd_format.drivers.BaseDriver.by_name if getattr(_d, 'compresion', False)]:
         cd = gpsd_format.drivers.get_compression(c.name)
         assert cd == c, "%r != %r" % (c, cd)
     try:
@@ -152,7 +152,7 @@ def test_get_compression():
 
 def test_detect_file_type():
 
-    for d in gpsd_format.drivers.REGISTERED_DRIVERS:
+    for d in [_d for _d in gpsd_format.drivers.BaseDriver.by_name if getattr(_d, 'compresion', False)]:
         for ext in d.extensions:
             rd = gpsd_format.drivers.detect_file_type('path.%s.ext' % ext)
             assert d == rd, "%r != %r" % (d, rd)
@@ -165,7 +165,7 @@ def test_detect_file_type():
 
 def test_detect_compression_type():
 
-    for c in gpsd_format.drivers.COMPRESSION_DRIVERS:
+    for c in [_d for _d in gpsd_format.drivers.BaseDriver.by_name if getattr(_d, 'compresion', False)]:
         print(c)
         print(c.extensions)
         for ext in c.extensions:
