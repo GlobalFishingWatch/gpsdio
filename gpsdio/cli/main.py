f0796ad892fabadab9c89b92c17e89472481efc2
Egil Moeller <egil.moller@freecode.no>
2015-06-04 11:57:28 +0200

    Support driver options in command line
    Use str2type for driver and compression options.  Added ctx.obj['r/w_opts'] for internal use.

--- gpsdio/cli/main.py
+++ gpsdio/cli/main.py
@@ -11,6 +11,7 @@ import sys
 
 import click
 import cligj.plugins
+import str2type.ext
 
 import gpsdio
 import gpsdio.drivers
@@ -42,8 +43,28 @@ import gpsdio.drivers
     help='Output compression format.  Normally auto-detected from file path.',
     type=click.Choice(list(gpsdio.drivers.BaseCompressionDriver.by_name.keys()))
 )
+@click.option(
+    '--i-drv-opt', '--ido', 'i_drv_opts', metavar='NAME=VAL', multiple=True,
+    callback=str2type.ext.click_cb_key_val,
+    help='Input driver options.  JSON values are automatically decoded.',
+)
+@click.option(
+    '--i-cmp-opt', '--ico', 'i_cmp_opts', metavar='NAME=VAL', multiple=True,
+    callback=str2type.ext.click_cb_key_val,
+    help='Input compression driver options.  JSON values are automatically decoded.',
+)
+@click.option(
+    '--o-drv-opt', '--odo', 'o_drv_opts', metavar='NAME=VAL', multiple=True,
+    callback=str2type.ext.click_cb_key_val,
+    help='Output driver options.  JSON values are automatically decoded.',
+)
+@click.option(
+    '--o-cmp-opt', '--oco', 'o_cmp_opts', metavar='NAME=VAL', multiple=True,
+    callback=str2type.ext.click_cb_key_val,
+    help='Output compression driver options.  JSON values are automatically decoded.',
+)
 @click.pass_context
-def main_group(ctx, i_drv, o_drv, i_cmp, o_cmp, verbose, quiet):
+def main_group(ctx, i_drv, o_drv, i_cmp, o_cmp, i_drv_opts, i_cmp_opts, o_drv_opts, o_cmp_opts, verbose, quiet):
     """
     A collection of tools for working with the GPSD JSON format (or the same format in a msgpack container)
     """
@@ -51,10 +72,34 @@ def main_group(ctx, i_drv, o_drv, i_cmp, o_cmp, verbose, quiet):
     verbosity = max(10, 30 - 10 * verbose) - quiet
     logging.basicConfig(stream=sys.stderr, level=verbosity)
 
+    # A collection of objects subcommands need access to
     ctx.obj = {
         'i_drv': i_drv,
         'o_drv': o_drv,
         'i_cmp': i_cmp,
         'o_cmp': o_cmp,
+        'i_drv_opts': i_drv_opts,
+        'i_cmp_opts': i_cmp_opts,
+        'o_drv_opts': o_drv_opts,
+        'o_cmp_opts': o_cmp_opts,
         'verbosity': verbosity
     }
+
+    # Subcommands can do `gpsdio.open(infile, **ctx.obj['r_opts'])` instead of picking
+    # individual
+    r_opts = {
+        'driver': ctx.obj['i_drv'],
+        'compression': ctx.obj['i_cmp'],
+        'do': ctx.obj['i_drv_opts'],
+        'co': ctx.obj['i_cmp_opts']
+    }
+    w_opts = {
+        'driver': ctx.obj['o_drv'],
+        'compression': ctx.obj['o_cmp'],
+        'do': ctx.obj['o_drv_opts'],
+        'co': ctx.obj['o_cmp_opts']
+    }
+    ctx.obj.update(
+        r_opts=r_opts,
+        w_opts=w_opts
+    )
