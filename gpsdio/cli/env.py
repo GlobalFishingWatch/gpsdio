9ee7f520f32fac615435aad2b7de4e14a800cf9e
Kevin Wurster <kevin@skytruth.org>
2015-08-07 17:15:02 -0400

    Moved info's --indent flag to options.py and re-worked gpsdio env to be more useful and incorporate info about the schema.

--- gpsdio/cli/env.py
+++ gpsdio/cli/env.py
@@ -3,57 +3,136 @@ gpsdio env
 """
 
 
-import doctest
-import logging
+import json
 
 import click
+import six
 
 import gpsdio
+from gpsdio.cli import options
 import gpsdio.drivers
 from gpsdio.drivers import registered_compression
 from gpsdio.drivers import registered_drivers
 
 
-@click.command()
-@click.option(
-    '--drivers', 'item', flag_value='drivers',
-    help="List of registered drivers and their I/O modes."
-)
-@click.option(
-    '--compression', 'item', flag_value='compression',
-    help='List of registered compression drivers and their I/O modes.'
-)
-@click.option(
-    '--driver-help', 'get_help', metavar='NAME',
-    help="Print a driver's documentation."
-)
-@click.option(
-    '--compression-help', 'get_help', metavar='NAME',
-    help="Print a compression driver's documentation."
-)
-@click.pass_context
-def env(ctx, item, get_help):
+@click.group()
+def env():
 
     """
     Information about the gpsdio environment.
     """
 
-    logger = logging.getLogger('gpsdio-cli-env')
-    logger.setLevel(ctx.obj['verbosity'])
-    logger.debug('Starting env')
 
-    if item == 'drivers':
-        for name, driver in gpsdio.drivers._BaseDriver.by_name.items():
-            click.echo("%s - %s" % (name, driver.io_modes))
-    elif item == 'compression':
-        for name, driver in gpsdio.drivers._BaseCompressionDriver.by_name.items():
-            click.echo("%s - %s" % (name, driver.io_modes))
-    elif get_help:
-        if get_help in registered_drivers:
-            click.echo(registered_drivers[get_help].__doc__)
-        elif get_help in registered_compression:
-            click.echo(registered_compression[get_help].__doc__)
-        else:
-            click.BadParameter("Unrecognized driver or compression: {}".format(get_help))
+@env.command(name='drivers', short_help="Print registered drivers and their I/O modes.")
+@click.argument('name', required=False)
+def drivers_cmd(name):
+
+    """
+    To get more information about a driver:
+    \b
+        $ gpsdio env drivers ${NAME}
+    """
+
+    if name:
+        try:
+            click.echo(registered_drivers[name].__doc__)
+        except Exception:
+            raise click.ClickException('Unrecognized driver: {}'.format(name))
     else:
-        raise click.BadParameter('A flag is required.')
+        for n, driver in gpsdio.drivers.registered_drivers.items():
+            click.echo("%s - %s" % (n, driver.io_modes))
+
+
+@env.command(name='compression', short_help="Print compression drivers and their I/O modes.")
+@click.argument('name', required=False)
+def drivers_cmd(name):
+
+    """
+    To get more information about a driver:
+    \b
+        $ gpsdio env compression ${NAME}
+    """
+
+    if name:
+        try:
+            click.echo(registered_compression[name].__doc__)
+        except Exception:
+            raise click.ClickException('Unrecognized driver: {}'.format(name))
+    else:
+        for n, driver in gpsdio.drivers.registered_compression.items():
+            click.echo("%s - %s" % (n, driver.io_modes))
+
+
+def _scrub_val(val):
+    types = (list, dict, str, unicode, int, float, bool, type(None)) + six.string_types
+    if isinstance(val, types):
+        return val
+    else:
+        return str(val)
+
+
+def _scrub_dict(dictionary):
+
+    """
+    Converts non-JSON serializable objects to strings.
+    """
+
+    if isinstance(dictionary, dict):
+        return {k: _scrub_dict(v) for k, v in dictionary.items()}
+    elif isinstance(dictionary, (list, tuple)):
+        return list(map(str, dictionary))
+    else:
+        return _scrub_val(dictionary)
+
+
+@env.command(name='schema', short_help="Information about the schema.")
+@click.argument('field', required=False)
+@options.indent_opt
+def schema_cmd(field, indent):
+
+    """
+    To get information about a specific field:
+    \b
+        $ gpsdio env schema ${FIELD}
+    """
+
+    if field:
+        try:
+            val = gpsdio.schema.CURRENT[field]
+        except Exception:
+            raise click.ClickException("Unrecognized field: {}".format(field))
+    else:
+        val = gpsdio.schema.CURRENT
+
+    click.echo(json.dumps(_scrub_dict(val), indent=indent, sort_keys=True))
+
+
+
+# @click.command()
+# )
+# @click.pass_context
+# def env(ctx, item, get_help):
+#
+#     """
+#     Information about the gpsdio environment.
+#     """
+#
+#     logger = logging.getLogger('gpsdio-cli-env')
+#     logger.setLevel(ctx.obj['verbosity'])
+#     logger.debug('Starting env')
+#
+#     if item == 'drivers':
+#         for name, driver in gpsdio.drivers._BaseDriver.by_name.items():
+#             click.echo("%s - %s" % (name, driver.io_modes))
+#     elif item == 'compression':
+#         for name, driver in gpsdio.drivers._BaseCompressionDriver.by_name.items():
+#             click.echo("%s - %s" % (name, driver.io_modes))
+#     elif get_help:
+#         if get_help in registered_drivers:
+#             click.echo(registered_drivers[get_help].__doc__)
+#         elif get_help in registered_compression:
+#             click.echo(registered_compression[get_help].__doc__)
+#         else:
+#             click.BadParameter("Unrecognized driver or compression: {}".format(get_help))
+#     else:
+#         raise click.BadParameter('A flag is required.')
