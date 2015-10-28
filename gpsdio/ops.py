8359fa375fcd87ae1e3d17d03e03a2655990edce
Kevin Wurster <kevin@skytruth.org>
2015-10-28 15:24:53 -0400

    Experimental gpsdio cat --geojson and gpsdio.ops.geojson().

--- gpsdio/ops.py
+++ gpsdio/ops.py
@@ -4,8 +4,12 @@ Message modification operations.
 
 
 import copy
+import warnings
+
+import six
 
 import gpsdio.schema
+from gpsdio.schema import export_msg
 
 
 def strip_msgs(stream, keep_invalid=False, invalid_key='__invalid__'):
@@ -39,3 +43,45 @@ def strip_msgs(stream, keep_invalid=False, invalid_key='__invalid__'):
             m[invalid_key] = invalid
 
         yield m
+
+
+def geojson(stream):
+
+    """
+    **Experimental**
+
+    Converts a stream of messages to GeoJSON features with point geometries.
+    Non-positional messages are dropped.
+
+    Currently, message type is ignored and only the lat/lon fields are used
+    to determine if a message is positional.  Ultimately output messages
+    will have the same schema but some internal validation work must first
+    be completed.
+
+    Parameters
+    ----------
+    stream : iter
+        GPSd messages.
+
+    Yields
+    ------
+    dict
+        GeoJSON features.
+    """
+
+    warnings.warn("gpsdio.ops.geojson() is experimental.")
+
+    for msg in stream:
+        lat = msg.get('lat')
+        lon = msg.get('lon')
+        if lat is not None and lon is not None:
+            yield {
+                'type': 'Feature',
+                'properties': {
+                    k: v for k, v in six.iteritems(export_msg(msg))
+                    if (k != 'lat' or k != 'lon')},
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': (lon, lat)
+                }
+            }
