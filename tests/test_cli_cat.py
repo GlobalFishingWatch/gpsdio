b1c7f5f0a3e1d6af69b4c6cb4e588b3b0fec5fb8
Kevin Wurster <kevin@skytruth.org>
2015-11-04 10:54:10 -0500

    Unittest for gpsdio cat

--- tests/test_cli_cat.py
+++ tests/test_cli_cat.py
@@ -3,24 +3,18 @@ Unittests for gpsdio cat
 """
 
 
-from click.testing import CliRunner
-import newlinejson as nlj
-import pytest
+import json
+import subprocess
 
 import gpsdio
 import gpsdio.cli.main
+import gpsdio.cli.cat
 
 
-
-@pytest.mark.skipif(True, reason="Click's CliRunner() class crashes on this test because "
-                                 "gpsdio is closing sys.stdout and CliRunner() expects to "
-                                 "do that itself.  Not sure what the work-around is.")
 def test_cat(types_msg_gz_path):
-    result = CliRunner().invoke(gpsdio.cli.main.main_group, [
-        'cat',
-        types_msg_gz_path
-    ])
-    assert result.exit_code is 0
+
+    result = subprocess.check_output(['gpsdio', 'cat', types_msg_gz_path]).decode('utf-8')
+    actual = (json.loads(l) for l in result.splitlines())
     with gpsdio.open(types_msg_gz_path) as expected:
-        for expected, actual in zip(expected, nlj.loads(result.output)):
-            assert expected == actual
+        for e, a in zip(expected, actual):
+            assert e == a
