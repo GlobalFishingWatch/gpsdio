9bcffc00c7bdc7040b2fd97aad70a992a2b2af3e
Kevin Wurster <kevin@skytruth.org>
2015-11-05 15:00:28 -0500

    Small improvements and removed python-dateutil

--- setup.py
+++ setup.py
@@ -6,60 +6,48 @@ Setup script for gpsdio
 """
 
 
+import codecs
 import os
-import sys
 
-from setuptools.command.test import test as TestCommand
 from setuptools import find_packages
 from setuptools import setup
 
 
-# https://pytest.org/latest/goodpractises.html
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ['tests']
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # Import here, because outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
-with open('README.rst') as f:
+with codecs.open('README.rst', encoding='utf-8') as f:
     readme = f.read().strip()
 
 
-version = None
-author = None
-email = None
-source = None
+def parse_dunder_line(string):
+
+    """
+    Take a line like:
+
+        "__version__ = '0.0.8'"
+
+    and turn it into a tuple:
+
+        ('__version__', '0.0.8')
+
+    Not very fault tolerant.
+    """
+
+    # Split the line and remove outside quotes
+    variable, value = (s.strip() for s in string.split('=')[:2])
+    value = value[1:-1].strip()
+    return variable, value
+
+
 with open(os.path.join('gpsdio', '__init__.py')) as f:
-    for line in f:
-        if line.strip().startswith('__version__'):
-            version = line.split('=')[1].strip().replace('"', '').replace("'", '')
-        elif line.strip().startswith('__author__'):
-            author = line.split('=')[1].strip().replace('"', '').replace("'", '')
-        elif line.strip().startswith('__email__'):
-            email = line.split('=')[1].strip().replace('"', '').replace("'", '')
-        elif line.strip().startswith('__source__'):
-            source = line.split('=')[1].strip().replace('"', '').replace("'", '')
-        elif None not in (version, author, email, source):
-            break
+    dunders = dict((parse_dunder_line(line) for line in f if line.strip().startswith('__')))
+    version = dunders['__version__']
+    author = dunders['__author__']
+    email = dunders['__email__']
+    source = dunders['__source__']
 
 
 setup(
     author=author,
     author_email=email,
-    cmdclass={'test': PyTest},
     description="A general purpose AIS I/O library using the GPSd AIVDM schema.",
     entry_points='''
         [console_scripts]
@@ -77,6 +65,7 @@ setup(
         'dev': [
             'pytest',
             'pytest-cov',
+            'coveralls'
         ]
     },
     install_requires=[
@@ -84,7 +73,6 @@ setup(
         'click-plugins',
         'msgpack-python',
         'newlinejson>=1.0',
-        'python-dateutil',
         'str2type>=0.4',
         'six>=1.8',
         'ujson',
